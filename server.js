ce4e1ad87f703edb9469787c7bf1a2cebeb53d5f
Mabdesaken <mathias.abdesaken@gmail.com>
2021-08-08 12:02:48 +0200

    First commit for web app

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+//Setting up index router for root off app
+const indexrouter = require('./routes/index')
+
+//views and layout settings
+app.set('view engine', 'ejs')
+app.set('views', __dirname + 'views')
+app.set('layout', 'layouts/layout')
+
+//app utility
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+//Telling app to use routes that are setup
+app.use('/', indexrouter)
+
+app.listen(process.env.PORT || 3003)
